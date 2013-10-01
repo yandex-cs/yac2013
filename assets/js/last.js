1c128469dafa1779b06d7c5fc54d933f66bed728
Pavel <php_prog@mail.ru>
2013-10-02 01:36:02 +0400

    teddy

--- assets/js/last.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function(evt){
-    jQuery("ul.nav.nav-list").find("#main").attr("class","active")
-    var src = jQuery("ul.nav.nav-list").find("#main").find('img').attr('src')
-    jQuery("ul.nav.nav-list").find("#main").find('img').attr('src', src.replace('.png', '-w.png'))
-})
