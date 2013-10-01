14a0f9166ecab7144df96109330558a2d1d58f86
Nasta <an.kartul@gmail.com>
2013-10-01 22:23:55 +0300

    optimization
    optimization

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,1 @@
-$(function(){
-    var menu = $("ul.nav.nav-list").find("#main"),
-        src = menu.find('img').attr('src');
-    menu.attr("class","active").find('img').attr('src', src.replace('.png', '-w.png'));
-});
+document.addEventListener('DOMContentLoaded',function(){var d = document.getElementById("main");d.className = "active";});
