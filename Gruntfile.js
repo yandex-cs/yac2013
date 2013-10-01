8d1e87650b9508bcf1b56c109a6e09311bf54f8f
Igor Savin <igor.savin.a@gmail.com>
2013-10-02 00:56:17 +0400

    +

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,19 +6,19 @@ module.exports = function(grunt) {
             my_target: {
                 files: {
                     'scripts.min.js': [
-                        'assets/js/bootstrap.js',
-                        'assets/js/bootstrap-affix.js',
-                        'assets/js/bootstrap-alert.js',
-                        'assets/js/bootstrap-button.js',
-                        'assets/js/bootstrap-carousel.js',
-                        'assets/js/bootstrap-collapse.js',
-                        'assets/js/bootstrap-dropdown.js',
-                        'assets/js/bootstrap-modal.js',
-                        'assets/js/bootstrap-popover.js',
-                        'assets/js/bootstrap-scrollspy.js',
-                        'assets/js/bootstrap-tab.js',
-                        'assets/js/bootstrap-tooltip.js',
-                        'assets/js/bootstrap-transition.js'//,
+                        'assets/js/bootstrap.js'//,
+                        //'assets/js/bootstrap-affix.js',
+                        //'assets/js/bootstrap-alert.js',
+                        //'assets/js/bootstrap-button.js',
+                        //'assets/js/bootstrap-carousel.js',
+                        //'assets/js/bootstrap-collapse.js',
+                        //'assets/js/bootstrap-dropdown.js',
+                        //'assets/js/bootstrap-modal.js',
+                        //'assets/js/bootstrap-popover.js',
+                        //'assets/js/bootstrap-scrollspy.js',
+                        //'assets/js/bootstrap-tab.js',
+                        //'assets/js/bootstrap-tooltip.js',
+                        //'assets/js/bootstrap-transition.js'//,
                         //'assets/js/bootstrap-typehead.js'
                     ]
                 }
