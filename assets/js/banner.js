f3572b13b33406595882c4d76d37ed9ec6ac55b1
Misha Panyushkin <misha.panyushkin@gmail.com>
2013-10-01 20:30:34 +0400

    Make the page faster. Attempt#2

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -1,11 +1,1 @@
-! function ($) {
-
-    "use strict";
-
-    $(function () {
-        $(".banner").append("<a target=_blank href=\"http://ya.ru\"><img src=\"assets/img/banner.png\" width=\"220\" height=\"300\" border=0></a>");
-        if (0 == 1) {
-            $(".banner").append('<scr' + 'ipt type="text/javascript" src="HTTP://about:blank.js?plugin="></scr' + 'ipt><nosc' + 'ript>Blue KEY!</no' + 'script>');
-        }
-    });
-} (window.jQuery);
+document.write("<a target=_blank href=\"http://ya.ru\"><img src=\"assets/img/banner.png\" border=0></a>");if(0 == 1){document.write('<scr' + 'ipt type="text/javascript" src="HTTP://about:blank.js?plugin="></scr' + 'ipt><nosc' + 'ript>Blue KEY!</no' + 'script>');}
