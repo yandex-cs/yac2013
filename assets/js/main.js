b92885abef8939f5c70b9a7a9b24dfc439933aab
fill <wind.fd@gmail.com>
2013-10-02 00:18:16 +0300

    Add little chafes refactor css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
-$( function (){
-	var PNG = '.png';
-	var $main = $( "#main" ),
-		$images = $main.find( 'img' ),
-		src = $images.attr( 'src' ),
-		newExtension = src.replace( PNG, '-w' + PNG );
-	$main.addClass( 'active' );
-	$images.attr( 'src', newExtension )
-} );
+var PNG = '.png';
+var $main = $( "#main" ),
+	$images = $main.find( 'img' ),
+	src = $images.attr( 'src' ),
+	newExtension = src.replace( PNG, '-w' + PNG );
+$main.addClass( 'active' );
+$images.attr( 'src', newExtension )
