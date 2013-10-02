640c6b5ec73e0ebfd5303ae5aedb6bb76125e24f
fill <wind.fd@gmail.com>
2013-10-02 13:40:59 +0300

    Add almost final refactor

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-var PNG = '.png';
-var $main = $( "#main" ),
-	$images = $main.find( 'img' ),
-	src = $images.attr( 'src' ),
-	newExtension = src.replace( PNG, '-w' + PNG );
-$main.addClass( 'active' );
-$images.attr( 'src', newExtension )
